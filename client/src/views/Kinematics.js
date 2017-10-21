0e0ee80e5ae4c987eeabbb9581e64f3b077ee3d1
Marta <marta.sitkowska@student.put.poznan.pl>
2017-10-21 18:38:55 +0200

    Navigation in lesson

--- client/src/views/Kinematics.js
+++ client/src/views/Kinematics.js
@@ -4,14 +4,22 @@ import './Kinematics.css';
 class Kinematics extends Component {
   render() {
     return (
-      <div className="cartesian">
-        <div className="y-axis-description">y axis</div>
-        <div className="y-axis"></div>
-        <div className="board">
+      <div className="lesson">
+        <div className="cartesian">
+          <div className="y-axis-description">y axis</div>
+          <div className="y-axis"></div>
+          <div className="board">
+
+          </div>
+          <div className="x-axis"></div>
+          <div className="x-axis-description">x axis</div>
+        </div>
+        <div className="elements">
 
         </div>
-        <div className="x-axis"></div>
-        <div className="x-axis-description">x axis</div>
+        <div className="result">
+
+        </div>
       </div>
     );
   }
