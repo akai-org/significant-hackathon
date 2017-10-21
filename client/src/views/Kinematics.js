ed33da473bce04a7b23b37c0d9e36373ee7992b6
Rafał Rudol <rrrrudol@gmail.com>
2017-10-21 20:46:07 +0200

    resolve images

--- client/src/views/Kinematics.js
+++ client/src/views/Kinematics.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Kinematics.css';
-
 const math = require('mathjs');
 
 class Result {
@@ -87,6 +86,8 @@ class Element extends Component {
   constructor(element){
     super();
 
+    console.log(this.props);
+
     this.data = element;
 
     for(const key in this.data) {
@@ -112,103 +113,10 @@ class Element extends Component {
 class Kinematics extends Component {
   constructor(){
     super();
-    this.state = {};
-    this.state.elementsArray = [];
-    //this.data = {};
 
-      const data = JSON.parse(`{
-    "Name":{
-        "taskname":"Plane Potato",
-        "author":"Adrian"
-    },
-    "Elements":[
-        {
-            "name":"Puf",
-            "imageUrl":"/images/Puf.png",
-            "xSize":"5%",
-            "ySize":"5%",
-            "xStart":"40%",
-            "yStart":"5%",
-            "isConstant":"false",
-            "xVelocity":"100",
-            "yVelocity":"0",
-            "x":"",
-            "y":""
-        },
-        {
-            "name":"Plane",
-            "imageUrl":"/images/Plane.png",
-            "xSize":"20%",
-            "ySize":"20%",
-            "xStart":"50%",
-            "yStart":"5%",
-            "isConstant":"false",
-            "xVelocity":"100",
-            "yVelocity":"0",
-            "x":"",
-            "y":""
-        },
-        {
-            "name":"Tomato",
-            "imageUrl":"/images/Tomato.png",
-            "xSize":"4%",
-            "ySize":"2%",
-            "xStart":"53%",
-            "yStart":"5%",
-            "isConstant":"false",
-            "xVelocity":"Plane.xVelocity",
-            "yVelocity":"Plane.yVelocity",
-            "x":"",
-            "y":""
-        },
-        {
-            "name":"Pot",
-            "imageUrl":"/images/Pot.png",
-            "xSize":"10%",
-            "ySize":"2%",
-            "xStart":"70%",
-            "yStart":"95%",
-            "isConstant":"true",
-            "xVelocity":"",
-            "yVelocity":"",
-            "x":"",
-            "y":""
-        }
-    ],
-    "Results":[
-        {
-            "leftSide":"Potato",
-            "relation":"isIn",
-            "rightSide":"Pot"
-        },
-        {
-            "leftSide":"Plane.y",
-            "relation":">",
-            "rightSide":"0"
-        }
-    ],
-    "Values":[
-        {
-            "name":"Hp",
-            "known":"false",
-            "place":"Plane.y",
-            "value":""
-        },
-        {
-            "name":"Vxp",
-            "known":"true",
-            "place":"Plane.xVelocity",
-            "value":""
-        },
-        {
-            "name":"X",
-            "known":"true",
-            "for":"Pot.x-Plane.x",
-            "value":""
-        }
-    ]
-}`);
-
+    this.state = {
+      elementsArray: []
+    };
   }
 
   componentWillMount() {
@@ -221,7 +129,7 @@ class Kinematics extends Component {
         });
         this.setState({elementsArray : elementsArray});
         this.forceUpdate();
-        
+
         console.log("printing resultsArray");
         let resultsArray = [];
         data.Results.forEach( (e) => {
@@ -236,6 +144,9 @@ class Kinematics extends Component {
 
   render() {
     if (!this.state.elementsArray[0]) return '';
+
+
+
     console.log(this.state);
     return (
       <div className="lesson">
