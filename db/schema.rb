7b409652da51a2c26262dd47f183200f586b5381
MichalDolata <dev@dolata.me>
2017-10-21 22:47:30 +0200

    Add seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171021190920) do
 
   create_table "active_admin_comments", force: :cascade do |t|
@@ -59,7 +58,6 @@ ActiveRecord::Schema.define(version: 20171021190920) do
     t.string "image_file_name"
     t.string "image_content_type"
     t.integer "image_file_size"
-   
     t.datetime "image_updated_at"
     t.index ["task_id"], name: "index_elements_on_task_id"
   end
