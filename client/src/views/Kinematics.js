9c7669a4066cd0b9282faa23408f6c0eccd86346
Marta <marta.sitkowska@student.put.poznan.pl>
2017-10-21 17:58:24 +0200

    Cartesian

--- client/src/views/Kinematics.js
+++ client/src/views/Kinematics.js
@@ -5,7 +5,13 @@ class Kinematics extends Component {
   render() {
     return (
       <div className="cartesian">
+        <div className="y-axis-description">y axis</div>
+        <div className="y-axis"></div>
+        <div className="board">
 
+        </div>
+        <div className="x-axis"></div>
+        <div className="x-axis-description">x axis</div>
       </div>
     );
   }
