4ca2c4216ec76a5cd36cf3c4cbcea88df9d222e4
Rafał Rudol <rrrrudol@gmail.com>
2017-10-21 15:54:35 +0200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Router, Route } from 'react-router'
 import logo from './logo.svg';
 import './App.css';
+
+import Main from './views/Main';
 
 class App extends Component {
   render() {
@@ -10,9 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Main />
       </div>
     );
   }
---
+++ client/src/views/Main.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Main = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+export default Main;
