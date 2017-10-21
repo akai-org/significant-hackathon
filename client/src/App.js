2b04b766d16d2bd0a4a89933b96bdc1162618a99
Rafał Rudol <rrrrudol@gmail.com>
2017-10-21 16:26:50 +0200

    Add empty Plane lesson and fix routing

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Main from './views/Main';
+import Naviagtion from './components/Navigation';
 
 /* Exercises */
 import Plane from './views/exercises/Plane';
@@ -15,12 +16,15 @@ import Plane from './views/exercises/Plane';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={Main}></Route>
-          <Route exact path="/plane" component={Plane}></Route>
-        </div>
-      </Router>
+      <div class="App">
+        <Router>
+          <div>
+            <Naviagtion />
+            <Route path="/" component={Main}></Route>
+            <Route path="/lesson/plane" component={Plane}></Route>
+          </div>
+        </Router>
+      </div>
     );
   }
 }
--- client/src/views/exercises/Plane.js
+++ client/src/views/exercises/Plane.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 const Plane = () => (
   <div>
     <h2>Plane</h2>
+    <div className="game">
+      <div className="plane"></div>
+    </div>
   </div>
 )
 
