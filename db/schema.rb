63b0683ac5f26fcfee7db835d53fc96e28b7b758
Haradd <blazej.piaskowski@gmail.com>
2017-10-21 15:56:22 +0200

    add UserProviders migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021123748) do
+ActiveRecord::Schema.define(version: 20171021135516) do
+
+  create_table "user_providers", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_providers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
